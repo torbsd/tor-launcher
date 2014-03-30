06d3404cc2483a54a4f88c780cfa1b67aaead24a
Mike Perry <mikeperry-git@torproject.org>
2014-03-29 20:54:19 -0700

    Add Turkish to bundle locales.

--- localization/import-translations.sh
+++ localization/import-translations.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BUNDLE_LOCALES="ar de es fa fr it ko nl pl pt ru vi zh-CN"
+BUNDLE_LOCALES="ar de es fa fr it ko nl pl pt ru tr vi zh-CN"
 
 # XXX: Basque (eu) by request in #10687.
 # This is not used for official builds, but should remain 
