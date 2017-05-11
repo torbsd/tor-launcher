485ba9456724e38b661c90dccc90322c74fa405f
Kathy Brade <brade@pearlcrescent.com>
2017-05-11 17:34:16 -0400

    By default, use TCP for the ControlPort and SocksPort.
    For the stable releases of Tor Browser, we want to continue to use
    TCP so that packages like TorBirdy that use the tor that is started
    by Tor Browser continue to work. Also see bugs 20111 and 20761.

--- src/defaults/preferences/prefs.js
+++ src/defaults/preferences/prefs.js
@@ -9,27 +9,27 @@ pref("extensions.torlauncher.loglevel", 4);  // 1=verbose, 2=debug, 3=info, 4=no
 pref("extensions.torlauncher.logmethod", 1);  // 0=stdout, 1=errorconsole, 2=debuglog
 pref("extensions.torlauncher.max_tor_log_entries", 1000);
 
-// By default, an IPC object (e.g., a Unix domain socket) at a default
-// location is used for the Tor control port.
-// Change control_port_use_ipc to false to use a TCP connection instead, as
-// defined by control_host and control_port.
-// Modify control_ipc_path to override the default IPC object location. If a
-// relative path is used, it is handled like torrc_path (see below).
-pref("extensions.torlauncher.control_port_use_ipc", true);
-pref("extensions.torlauncher.control_ipc_path", "");
+// By default, Tor Launcher configures a TCP listener for the Tor
+// control port, as defined by control_host and control_port.
+// Set control_port_use_ipc to true to use an IPC object (e.g., a Unix
+// domain socket) instead. You may also modify control_ipc_path to
+// override the default IPC object location. If a relative path is used,
+// it is handled like torrc_path (see below).
 pref("extensions.torlauncher.control_host", "127.0.0.1");
 pref("extensions.torlauncher.control_port", 9151);
+pref("extensions.torlauncher.control_port_use_ipc", false);
+pref("extensions.torlauncher.control_ipc_path", "");
 
-// By default, an IPC object (e.g., a Unix domain socket) at a default
-// location is used for the Tor SOCKS port.
-// Change socks_port_use_ipc to false to use a TCP connection. When a
-// TCP connection is used, the host is taken from the network.proxy.socks
-// pref and the port is taken from the network.proxy.socks_port pref.
-// Modify socks_ipc_path to override the default IPC object location. If a
-// relative path is used, it is handled like torrc_path (see below).
+// By default, Tor Launcher configures a TCP listener for the Tor
+// SOCKS port. The host is taken from the network.proxy.socks pref and
+// the port is taken from the network.proxy.socks_port pref.
+// Set socks_port_use_ipc to true to use an IPC object (e.g., a Unix
+// domain socket) instead. You may also modify socks_ipc_path to
+// override the default IPC object location. If a relative path is used,
+// it is handled like torrc_path (see below).
 // Modify socks_port_flags to use a different set of SocksPort flags (but be
 // careful).
-pref("extensions.torlauncher.socks_port_use_ipc", true);
+pref("extensions.torlauncher.socks_port_use_ipc", false);
 pref("extensions.torlauncher.socks_ipc_path", "");
 pref("extensions.torlauncher.socks_port_flags", "IPv6Traffic PreferIPv6 KeepAliveIsolateSOCKSAuth");
 
